ad53a28d2167e4e93f4014c22a72a33e73e38c35
nawazish-qaavi78 <nawazishqaavi4006@gmail.com>
2025-03-28 14:07:05 +0530

    calculate raw

--- components/IRT.v
+++ components/IRT.v
@@ -8,11 +8,14 @@ module IRT #(
 );
 
 	integer i;
+	integer j;
 	
 	wire [regnum-1:0] current_rs = (rs1 ? {1<<rs1} : {regnum{1'b0}}) | (rs2 ? {1<<rs2} : {regnum{1'b0}});
 	
 	reg [regnum-1:0] IRT_RD [0:bs-1];
 	reg [regnum-1:0] IRT_RS [0:bs-1];
+	
+	reg [0:bs-1] raw = {bs{1'b1}};
 	
 	// by default all are 1, since that will mean no independent instruction detected yet
 	initial begin
@@ -29,6 +32,21 @@ module IRT #(
 		end else begin
 			IRT_RD[buffer_index] <= rd ? {1<<rd} : {regnum{1'b0}};
 			IRT_RS[buffer_index] <= current_rs;
+		end
+	end
+	
+	always@(*) begin
+		if(rst) begin
+			raw = {bs{1'b1}};
+		end else begin
+			for(j=0; j<bs; j=j+1) begin
+				if(j != buffer_index) begin
+					// what i'm doing is: looping through each instruction, if somewhere i match such that i'm writing, i.e. irt_rd[j][r] = 1, that means i'm dependent on that instruciton
+					raw[j] = IRT_RD[j][rs1] | IRT_RD[j][rs2]; 
+				end else begin
+					raw[j] = 1'b0; // since an instruction can't be dependent on it self
+				end
+			end
 		end
 	end
 	
